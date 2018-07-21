f45146fc912b2875249137c27078f230c7572eef
nJcx <166627276469@qq.com>
2018-07-21 17:40:33 +0800

    添加注释以及变量重新命名

--- main_app/core/func/func.py
+++ main_app/core/func/func.py
@@ -1,29 +1,44 @@
 # -*- coding: utf-8 -*-
 from __future__ import division
-import tushare as ts
+
 import requests
 from conf.settings import tonghuashun_api,global_api,tonghuashun_post_search_api
 import datetime
 import time
 
 s = requests.Session()
+# 开启一个session
+
 def get(url,data = None,header = None):
+    '''
+    :param url: URL
+    :param data: 参数
+    :param header: 请求头
+    :return: 未处理的response
+    '''
     params = data
     if not header:
         header = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.15 Safari/537.36'}
-        r=s.get(url,headers=header,params=params)
+        response=s.get(url,headers=header,params=params)
     else:
-        r = s.get(url,headers=header,params=params)
-    return r
+        response = s.get(url,headers=header,params=params)
+    return response
 
 def post(url,data = None,header = None):
+    '''
+    :param url: URL
+    :param data: 参数
+    :param header: 请求头
+    :return: 未处理的response
+    '''
+
     data = data
     if not header:
         header = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.15 Safari/537.36'}
-        r=s.post(url,headers=header,data=data)
+        response =s.post(url,headers=header,data=data)
     else:
-        r = s.post(url, headers=header, data=data)
-    return r
+        response = s.post(url, headers=header, data=data)
+    return response
 
 
 def updown():
@@ -32,7 +47,7 @@ def updown():
     api = tonghuashun_api
     try:
         r=get(api).json()
-        print r
+        print(r)
         up = r.get('zdfb_data').get('znum')
         "上涨数"
         down = r.get('zdfb_data').get('dnum')
@@ -54,39 +69,39 @@ def updown():
         ratio['cant_down'] = cant_down
         ratio['level'] = level
         ratio['uptodown'] = uptodown
-    except Exception,e:
-        print str(e)
+    except Exception as e:
+        print(e)
     return ratio
 
 
-def cant_item():
-    "涨停和跌停的票"
-    cant_item ={}
-    try:
-        today = ts.get_today_all()
-        print today
-    except Exception, e:
-        print str(e)
-    return cant_item
-
-
 def global_index():
-    '指数'
+
+    '''
+    大盘指数
+    :return:
+    '''
+
     global_index = []
     try:
         try:
             r = get(global_api).json()
             global_index = r.get(u'中国').get('dataArr')
-        except Exception,e:
+        except Exception as e:
             r = get(global_api).json()
             global_index = r.get(u'中国').get('dataArr')
-    except Exception,e:
+    except Exception as e:
         r = get(global_api).json()
         global_index = r.get(u'中国').get('dataArr')
     return global_index
 
 
 def pagination(perpage=70):
+
+    '''
+    :param perpage:  同花顺单页返回item个数
+    :return:
+    '''
+
     try:
         header = {
             'hexin-v': 'Ah9Nj1sjuPkzUrzl7_tej-BvqHiqhHPGjdl3FrFsursWKDFuuVQDdp2oB2fC',
@@ -102,29 +117,38 @@ def pagination(perpage=70):
                   '[%22%22,%22%22,%22onTable%22,%22onTable%22,%22onTable%22,%22onTable%22,%22onTable%22,%22onTable%22,' \
                   '%22onTable%22,%22onTable%22,%22onTable%22,%22onTable%22,%22onTable%22,%22onTable%22,%22onTable%22,%22onTable%22]'.format(token=token,perpage=perpage)
         get(get_url,header =header)
-    except Exception,e:
+    except Exception:
         return False
     return True
 
 
 
 def capital_flow():
+    '''
+    资金流
+    :return:
+    '''
     capital = []
     try:
         data = {
             'tid': 'stockpick',
             'w': u'大单流入大于1000万，流通市值小于51亿',
             'robot': '{"source":"Ths_iwencai_Xuangu","user_id":"425674582","log_info":"{\"other_info\":\"{\\\"eventId\\\":\\\"iwencai_pc_send_click\\\",\\\"ct\\\":1529713684261}\"}","user_name":"mo_425674582","version":"1.3"}'}
-        xx = post(tonghuashun_post_search_api, data).json()
-        xxx = xx.get('data').get('wencai_data').get('result').get('result')
-        for x in xxx:
-            capital.append([x[0],x[1],x[3],x[-3]])
-    except Exception,e:
+        response = post(tonghuashun_post_search_api, data).json()
+        result = response.get('data').get('wencai_data').get('result').get('result')
+        for item in result:
+            capital.append([item[0],item[1],item[3],item[-3]])
+    except Exception:
         return False
     return capital
 
 
 def cant_down_item():
+
+    '''
+    当日跌停
+    :return:
+    '''
 
     cant_down = {}
     try:
@@ -135,14 +159,19 @@ def cant_down_item():
                 "log_info": "{\"other_info\":\"{\\\"eventId\\\":\\\"iwencai_pc_send_click\\\",\\\"ct\\\":1529236182583}\"}",
                 "user_name": "mo_425674582", "version": "1.3"}
             }
-        r = post(tonghuashun_post_search_api,data=data).json()
-        xx = r.get('data').get('wencai_data').get('result').get('result')
-    except Exception,e:
+        response = post(tonghuashun_post_search_api,data=data).json()
+        result= response.get('data').get('wencai_data').get('result').get('result')
+    except Exception:
         return False
     return cant_down
 
 
 def cant_up_item():
+    '''
+    当日涨停
+    :return:
+    '''
+
     up_list = []
     try:
         data = {
@@ -152,17 +181,21 @@ def cant_up_item():
                 "log_info": "{\"other_info\":\"{\\\"eventId\\\":\\\"iwencai_pc_send_click\\\",\\\"ct\\\":1529236182583}\"}",
                 "user_name": "mo_425674582", "version": "1.3"}
             }
-        r = post(tonghuashun_post_search_api,data=data).json()
-        xx = r.get('data').get('wencai_data').get('result').get('result')
-        for x in xx:
-            up_list.append([x[0],x[1],x[-8],x[-7]])
-    except Exception,e:
+        response = post(tonghuashun_post_search_api,data=data).json()
+        result = response.get('data').get('wencai_data').get('result').get('result')
+        for item in result:
+            up_list.append([item[0],item[1],item[-8],item[-7]])
+    except Exception:
         return False
     return up_list
 
 
 def buyday():
-    '交易时间'
+    '''
+    判断是否是交易时间
+    :return:
+    '''
+
     worktime=['09:25:00','11:30:00','13:00:00','15:00:00']
     dayofweek = datetime.datetime.now().weekday()
     beginwork=datetime.datetime.now().strftime("%Y-%m-%d")+' '+worktime[0]
@@ -203,6 +236,6 @@ def suggest(r):
         return u'涨跌比：%s 建议空仓' %r
 
 if __name__ == '__main__':
-    print updown()
+    updown()
 
 
